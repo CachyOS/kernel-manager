81984a7efcc05915c0133ea2615ffd38fb1ebea8
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2022-10-17 20:51:16 +0400

    👷 fix compilation

--- src/mkoptions.py
+++ src/mkoptions.py
@@ -52,8 +52,23 @@ def main():
         header.write(globals()['__doc__'])
         header.write("""#pragma once
 
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wuseless-cast"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#endif
+
 #include <frozen/unordered_map.h>
 #include <frozen/string.h>
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 
 #include <string_view>
 
