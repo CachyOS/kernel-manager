63cb199ff5d00c8969d33da3da60464f5b3a4990
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2023-03-12 21:35:21 +0400

    👷 add translations

---
+++ lang/cachyos-kernel-manager_ru.ts
@@ -0,0 +1,319 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>ConfOptionsPage</name>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="33"/>
+        <source>Enable CachyOS config</source>
+        <translation>Включить CachyOS конфиг</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="62"/>
+        <source>Tweak kernel options prior to a build via nconfig</source>
+        <translation>Настройте параметры ядра перед сборкой с помощью nconfig</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="91"/>
+        <source>Tweak kernel options prior to a build via menuconfig</source>
+        <translation>Настройте параметры ядра перед сборкой с помощью menuconfig</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="120"/>
+        <source>Tweak kernel options prior to a build via xconfig</source>
+        <translation>Настройте параметры ядра перед сборкой с помощью xconfig</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="149"/>
+        <source>Tweak kernel options prior to a build via gconfig</source>
+        <translation>Настройте параметры ядра перед сборкой с помощью gconfig</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="178"/>
+        <source>Disable NUMA</source>
+        <translation>Отключить NUMA</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="207"/>
+        <source>Use Modprobed-db</source>
+        <translation>Использовать Modprobed-db</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="236"/>
+        <source>Enable KBUILD_CFLAGS -O3</source>
+        <translation>Включить KBUILD_CFLAGS -O3</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="265"/>
+        <source>Set performance governor as default</source>
+        <translation>Установить performance governor  по умолчанию</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="294"/>
+        <source>Enable TCP_CONG_BBR2</source>
+        <translation>Включить TCP_CONG_BBR2</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="323"/>
+        <source>Running tick rate</source>
+        <translation>Текущий tick rate</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="352"/>
+        <source>Select tickless</source>
+        <translation>Выбрать tickless</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="381"/>
+        <source>Select preempt</source>
+        <translation>Выбрать preempt</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="410"/>
+        <source>Disable MQ-Deadline I/O scheduler</source>
+        <translation>Отключить планировщик I/O MQ-Deadline</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="439"/>
+        <source>Disable Kyber I/O scheduler</source>
+        <translation>Отключить планировщик I/O Kyber</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="468"/>
+        <source>Enable multigenerational LRU</source>
+        <translation>Включить multigenerational LRU</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="497"/>
+        <source>Enable per-VMA locking</source>
+        <translation>Включить блокировку для каждого VMA</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="526"/>
+        <source>Enable DAMON</source>
+        <translation>Включить DAMON</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="555"/>
+        <source>Enable Linux Random Number Generator</source>
+        <translation>Включить генератор случайных чисел Linux</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="584"/>
+        <source>CPU compiler optimizations</source>
+        <translation>Оптимизация для процессора</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="613"/>
+        <source>Apply automatic CPU Optimization</source>
+        <translation>Применяйте автоматическую оптимизацию процессора</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="642"/>
+        <source>Disable debug to lower the size of the kernel</source>
+        <translation>Отключить debug, чтобы уменьшить размер ядра</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="671"/>
+        <source>Enable zram/zswap ZSTD compression</source>
+        <translation>Включить сжатие zram/zswap ZSTD</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="700"/>
+        <source>Selecting the ZSTD kernel and modules compression level</source>
+        <translation>Выбор уровня сжатия ядра и модулей ZSTD</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="729"/>
+        <source>Enable LTO</source>
+        <translation>Включить LTO</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="758"/>
+        <source>Build the ZFS module</source>
+        <translation>Собрать модуль ZFS</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="787"/>
+        <source>Build the BCACHEFS module</source>
+        <translation>Собрать модуль BCACHEFS</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="816"/>
+        <source>Enable RT kernel</source>
+        <translation>Включить ядро RT</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="845"/>
+        <source>Enable LATENCY NICE</source>
+        <translation>Включить LATENCY NICE</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="874"/>
+        <source>Transparent Hugepages</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="933"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="940"/>
+        <source>Execute</source>
+        <translation>Выполнить</translation>
+    </message>
+</context>
+<context>
+    <name>ConfPatchesPage</name>
+    <message>
+        <location filename="../src/conf-patches-page.ui" line="81"/>
+        <source>Add remote patch</source>
+        <translation>Добавить патч из интернета</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-patches-page.ui" line="88"/>
+        <source>Add local patch</source>
+        <translation>Добавить локальный патч</translation>
+    </message>
+</context>
+<context>
+    <name>ConfWindow</name>
+    <message>
+        <location filename="../src/conf-window.ui" line="17"/>
+        <source>CachyOS Kernel Manager Configure</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.ui" line="40"/>
+        <source>Options</source>
+        <translation>Опции</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.ui" line="45"/>
+        <source>Patches</source>
+        <translation>Патчи</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="379"/>
+        <source>BMQ - BitMap Queue CPU scheduler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="380"/>
+        <source>Bore - Burst-Oriented Response Enhancer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="381"/>
+        <source>CFS - Completely Fair Scheduler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="382"/>
+        <source>Hardened - Hardened kernel with the BORE Scheduler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="383"/>
+        <source>PDS - Priority and Deadline based Skip list multiple queue CPU scheduler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="384"/>
+        <source>RC - Release Candidate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="385"/>
+        <source>TT - Task Type Scheduler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="503"/>
+        <source>Select one or more patch files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="505"/>
+        <source>Patch file (*.patch)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="521"/>
+        <source>Enter URL patch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="522"/>
+        <source>Patch URL:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/km-window.ui" line="17"/>
+        <source>CachyOS Kernel Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="28"/>
+        <source>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
+&lt;p&gt;You can install and/or uninstall kernel packages using the checkboxes on the leftmost column.&lt;/p&gt;
+&lt;p&gt;This app won&apos;t work, if you are already running a pacman instance.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="64"/>
+        <source>Choose</source>
+        <translation>Выбор</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="69"/>
+        <source>PkgName</source>
+        <translation>Имя пакета</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="74"/>
+        <source>Version</source>
+        <translation>Версия</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="79"/>
+        <source>Category</source>
+        <translation>Категория</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="103"/>
+        <source>nothing here..                                                                                      </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="139"/>
+        <source>Configure</source>
+        <translation>Настроить</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="153"/>
+        <source>Execute</source>
+        <translation>Выполнить</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="146"/>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="586"/>
+        <source>No kernels found!
+Please run `pacman -Sy` to update DB!
+This is needed for the app to work properly</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
