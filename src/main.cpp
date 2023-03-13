627578309c0a2f0fac86bc18f568c1f914cb1d44
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2023-03-14 01:49:19 +0400

    🚧 fix compilation

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,23 @@
 #include <QApplication>
 #include <QSharedMemory>
 #include <QTranslator>
+
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#pragma clang diagnostic ignored "-Wimplicit-int-conversion"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#endif
+
 #include <QLibraryInfo>
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 
 static bool IsInstanceAlreadyRunning(QSharedMemory& memoryLock) {
     if (!memoryLock.create(1)) {
