a0ca2f39238c5707d1a0b89a8507ff941b529fdc
Piotr Gorski <lucjan.lucjanov@gmail.com>
2023-07-08 00:12:17 +0200

    Add a Polish translation
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

---
+++ lang/cachyos-kernel-manager_pl.ts
@@ -0,0 +1,256 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl">
+<context>
+    <name>ConfOptionsPage</name>
+    <message>
+        <source>Enable CachyOS config</source>
+        <translation>Włącz modyfikacje CachyOS</translation>
+    </message>
+    <message>
+        <source>Tweak kernel options prior to a build via nconfig</source>
+        <translation>Dostosuj opcje budowania kernela za pomocą nconfig</translation>
+    </message>
+    <message>
+        <source>Tweak kernel options prior to a build via menuconfig</source>
+        <translation>Dostosuj opcje budowania kernela za pomocą menuconfig</translation>
+    </message>
+    <message>
+        <source>Tweak kernel options prior to a build via xconfig</source>
+        <translation>Dostosuj opcje budowania kernela za pomocą xconfig</translation>
+    </message>
+    <message>
+        <source>Tweak kernel options prior to a build via gconfig</source>
+        <translation>Dostosuj opcje budowania kernela za pomocą gconfig</translation>
+    </message>
+    <message>
+        <source>Disable NUMA</source>
+        <translation>Wyłącz NUMA</translation>
+    </message>
+    <message>
+        <source>Use Modprobed-db</source>
+        <translation>Użyj Modprobed-db</translation>
+    </message>
+    <message>
+        <source>Enable KBUILD_CFLAGS -O3</source>
+        <translation>Aktywuj KBUILD_CFLAGS -O3</translation>
+    </message>
+    <message>
+        <source>Set performance governor as default</source>
+        <translation>Wybierz zarządcę "Performance" jako domyślny/translation>
+    </message>
+    <message>
+        <source>Enable TCP_CONG_BBR2</source>
+        <translation>Aktywuj TCP_CONG_BBR2</translation>
+    </message>
+    <message>
+        <source>Running tick rate</source>
+        <translation>Wybierz "rick rate"</translation>
+    </message>
+    <message>
+        <source>Select tickless</source>
+        <translation>Wybierz "tickless"</translation>
+    </message>
+    <message>
+        <source>Select preempt</source>
+        <translation>Wybierz "preempt"</translation>
+    </message>
+    <message>
+        <source>Disable MQ-Deadline I/O scheduler</source>
+        <translation>Wyłącz planistę I/O MQ-Deadline </translation>
+    </message>
+    <message>
+        <source>Disable Kyber I/O scheduler</source>
+        <translation>Wyłącz planistę I/O Kyber</translation>
+    </message>
+    <message>
+        <source>Enable multigenerational LRU</source>
+        <translation>Aktywuj Multigenerational LRU</translation>
+    </message>
+    <message>
+        <source>Enable per-VMA locking</source>
+        <translation>Aktywuj per-VMA locking</translation>
+    </message>
+    <message>
+        <source>Enable DAMON</source>
+        <translation>Aktywuj DAMON</translation>
+    </message>
+    <message>
+        <source>Enable Linux Random Number Generator</source>
+        <translation>Aktywuj LRNG</translation>
+    </message>
+    <message>
+        <source>CPU compiler optimizations</source>
+        <translation>Wybierz optymalizacje CPU</translation>
+    </message>
+    <message>
+        <source>Apply automatic CPU Optimization</source>
+        <translation>Zastosuj automatyczne optymalizacje CPU</translation>
+    </message>
+    <message>
+        <source>Disable debug to lower the size of the kernel</source>
+        <translation>Wyłącz debugowanie w celu zmniejszenia rozmiaru kernela</translation>
+    </message>
+    <message>
+        <source>Enable LTO</source>
+        <translation>Włącz LTO</translation>
+    </message>
+    <message>
+        <source>Build the ZFS module</source>
+        <translation>Zbuduj moduł ZFS</translation>
+    </message>
+    <message>
+        <source>Build the BCACHEFS module</source>
+        <translation>Zbuduj moduł BCACHEFS</translation>
+    </message>
+    <message>
+        <source>Transparent Hugepages</source>
+        <translation>Transparente Hugepages</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>Wykonaj</translation>
+    </message>
+</context>
+<context>
+    <name>ConfPatchesPage</name>
+    <message>
+        <source>Add remote patch</source>
+        <translation>Dodaj zdalny patch</translation>
+    </message>
+    <message>
+        <source>Add local patch</source>
+        <translation>Dodaj lokalny patch</translation>
+    </message>
+</context>
+<context>
+    <name>ConfWindow</name>
+    <message>
+        <source>CachyOS Kernel Manager Configure</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Opcje</translation>
+    </message>
+    <message>
+        <source>Patches</source>
+        <translation>Patches</translation>
+    </message>
+    <message>
+        <source>BMQ - BitMap Queue CPU scheduler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bore - Burst-Oriented Response Enhancer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>CFS - Completely Fair Scheduler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hardened - Hardened kernel with the BORE Scheduler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>PDS - Priority and Deadline based Skip list multiple queue CPU scheduler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>RC - Release Candidate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>TT - Task Type Scheduler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select one or more patch files</source>
+        <translation>Wybierz jeden lub więcej patchy</translation>
+    </message>
+    <message>
+        <source>Patch file (*.patch)</source>
+        <translation>Plik Patch (*.patch)</translation>
+    </message>
+    <message>
+        <source>Enter URL patch</source>
+        <translation>Wprowadź URL</translation>
+    </message>
+    <message>
+        <source>Patch URL:</source>
+        <translation>Patch URL:</translation>
+    </message>
+    <message>
+        <source>CachyOS - Bore + EEVDF</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>RT - Realtime kernel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>CachyOS Kernel Manager</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
+&lt;p&gt;You can install and/or uninstall kernel packages using the checkboxes on the leftmost column.&lt;/p&gt;
+&lt;p&gt;This app won&apos;t work, if you are already running a pacman instance.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</source>
+        <translation>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Tutaj znajdziesz informację na temat aktualnie zainstalowanego kernela jak również kerneli dostępnych do zainstalowania.&lt;/p&gt;
+&lt;p&gt;Możesz zainstalować i/lub odinstalować pakiety jądra za pomocą pól wyboru w kolumnie po lewej stronie.&lt;/p&gt;
+&lt;p&gt;Ta aplikacja nie będzie działać, jeśli używasz już instancji pacmana.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Choose</source>
+        <translation>Wybór</translation>
+    </message>
+    <message>
+        <source>PkgName</source>
+        <translation>Nazwa pakietu</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Wersja</translation>
+    </message>
+    <message>
+        <source>Category</source>
+        <translation>Kategoria</translation>
+    </message>
+    <message>
+        <source>Configure</source>
+        <translation>Konfiguracja</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>Wykonaj</translation>
+    </message>
+    <message>
+        <source>No kernels found!
+Please run `pacman -Sy` to update DB!
+This is needed for the app to work properly</source>
+        <translation>Keine Kernel gefunden!
+Proszę wykonać `pacman -Sy` aby zaktualizować bazę danych!
+Jest to wymagana do prawidłowego działania pakietu</translation>
+    </message>
+</context>
+</TS>
