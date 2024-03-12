e7130c560e9bfd704f86cf9382f4baf43d02b141
Adjudicator069 <162614103+Adjudicator069@users.noreply.github.com>
2024-03-12 21:02:03 +0300

    CachyOS Kernel Manager Turkish localisation (#8)
    * CachyOS Kernel Manager Turkish localisation
    
    Any feedback or criticism is valued.
    
    * Finalised cachyos-kernel-manager_tr.ts
    
    Minor tweaks like rewording sentences and adjusting a few agent and locatum verbs for proper and common nouns to which they're subject to, wherever they're allowed, in order for them to sound more natural in the target language, without unnecessary pointers and symbols like asterisks and hyphens.
    
    * Minor update: cachyos-kernel-manager_tr.ts
    
    One of the translated sections in my draft was missing from the pushed iteration for some reason. And the letter '-n' was missing from "<translation>RC…" as well.

---
+++ lang/cachyos-kernel-manager_tr.ts
@@ -0,0 +1,272 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>ConfOptionsPage</name>
+    <message>
+        <source>Enable CachyOS config</source>
+        <translation>CachyOS ayarlamayı etkinleştir</translation>
+    </message>
+    <message>
+        <source>Tweak kernel options prior to a build via nconfig</source>
+        <translation>nconfig ile yapılandırmadan önce çekirdek ayarlarına ince ayar yap</translation>
+    </message>
+    <message>
+        <source>Tweak kernel options prior to a build via menuconfig</source>
+        <translation>menuconfig ile yapılandırmadan önce çekirdek ayarlarına ince ayar yap</translation>
+    </message>
+    <message>
+        <source>Tweak kernel options prior to a build via xconfig</source>
+        <translation>xconfig ile yapılandırmadan önce çekirdek ayarlarına ince ayar yap</translation>
+    </message>
+    <message>
+        <source>Tweak kernel options prior to a build via gconfig</source>
+        <translation>gconfig ile yapılandırmadan önce çekirdek ayarlarına ince ayar yap</translation>
+    </message>
+    <message>
+        <source>Disable NUMA</source>
+        <translation>NUMA devre dışı bırakılsın</translation>
+    </message>
+    <message>
+        <source>Use Modprobed-db</source>
+        <translation>Modprobed-db kullanılsın</translation>
+    </message>
+    <message>
+        <source>Enable KBUILD_CFLAGS -O3</source>
+        <translation>KBUILD_CFLAGS -O3 etkinleştirilsin</translation>
+    </message>
+    <message>
+        <source>Set performance governor as default</source>
+        <translation>Performans odaklı işlemci yöneticisini varsayılan yap</translation>
+    </message>
+    <message>
+        <source>Enable TCP_CONG_BBR3</source>
+        <translation>TCP_CONG_BBR3 etkinleştirilsin</translation>
+    </message>
+    <message>
+        <source>Running tick rate</source>
+        <translation>Çalışılan tik oranı</translation>
+    </message>
+    <message>
+        <source>Select tickless</source>
+        <translation>Tiksiz seçim yap</translation>
+    </message>
+    <message>
+        <source>Select preempt</source>
+        <translation>preempt belirle</translation>
+    </message>
+    <message>
+        <source>Enable multigenerational LRU</source>
+        <translation>Multigenerational LRU etkinleştirilsin</translation>
+    </message>
+    <message>
+        <source>Enable per-VMA locking</source>
+        <translation>per-VMA locking etkinleştirilsin</translation>
+    </message>
+    <message>
+        <source>Enable DAMON</source>
+        <translation>DAMON etkinleştirilsin</translation>
+    </message>
+    <message>
+        <source>Enable Linux Random Number Generator</source>
+        <translation>Linux Rastgele Sayı Üretecini etkinleştir</translation>
+    </message>
+    <message>
+        <source>CPU compiler optimizations</source>
+        <translation>İşlemci derleme iyileştirmeleri</translation>
+    </message>
+    <message>
+        <source>Apply automatic CPU Optimization</source>
+        <translation>Otomatik işlemci iyileştirmeleri uygulansın</translation>
+    </message>
+    <message>
+        <source>Enable LTO</source>
+        <translation>LTO etkinleştirilsin</translation>
+    </message>
+    <message>
+        <source>Build the ZFS module</source>
+        <translation>ZFS modülünü yapılandır</translation>
+    </message>
+    <message>
+        <source>Build the BCACHEFS module</source>
+        <translation>BCACHEFS modülünü yapılandır</translation>
+    </message>
+    <message>
+        <source>Transparent Hugepages</source>
+        <translation>Saydam HugePage'ler</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Vazgeç</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>Gerçekleştir</translation>
+    </message>
+    <message>
+        <source>Custom package name</source>
+        <translation>Özel paket ismi</translation>
+    </message>
+    <message>
+        <source>$pkgbase</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Build the NVIDIA module</source>
+        <translation>NVIDIA modülünü yapılandır</translation>
+    </message>
+</context>
+<context>
+    <name>ConfPatchesPage</name>
+    <message>
+        <source>Add remote patch</source>
+        <translation>Uzaktan yama ekle</translation>
+    </message>
+    <message>
+        <source>Add local patch</source>
+        <translation>Yerelden yama ekle</translation>
+    </message>
+</context>
+<context>
+    <name>ConfWindow</name>
+    <message>
+        <source>CachyOS Kernel Manager Configure</source>
+        <translation>CachyOS Çekirdek Yöneticisi Yapılandırma</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Seçenekler</translation>
+    </message>
+    <message>
+        <source>Patches</source>
+        <translation>Yamalar</translation>
+    </message>
+    <message>
+        <source>Bore - Burst-Oriented Response Enhancer</source>
+        <translation>BORE - Ani Yüke Odaklı Tepki Arttırıcı</translation>
+    </message>
+    <message>
+        <source>RC - Release Candidate</source>
+        <translation>RC - Çıkış sürümü adayı</translation>
+    </message>
+    <message>
+        <source>Select one or more patch files</source>
+        <translation>Bir veya daha fazla yama dosyası seç</translation>
+    </message>
+    <message>
+        <source>Patch file (*.patch)</source>
+        <translation>Yama dosyası (*.patch)</translation>
+    </message>
+    <message>
+        <source>Enter URL patch</source>
+        <translation>URL yaması gir</translation>
+    </message>
+    <message>
+        <source>Patch URL:</source>
+        <translation>Yama için URL:</translation>
+    </message>
+    <message>
+        <source>RT - Realtime kernel</source>
+        <translation>RT - Gerçek zamanlı çekirdek</translation>
+    </message>
+    <message>
+        <source>CachyOS - Bore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>RT-Bore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sched-Ext - BPF extensible scheduler class</source>
+        <translation>Sched-Ext - BPF genişletilebilir zamanlayıcı sınıfı</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>CachyOS Kernel Manager</source>
+        <translation>CachyOS Çekirdek Yöneticisi</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
+&lt;p&gt;You can install and/or uninstall kernel packages using the checkboxes on the leftmost column.&lt;/p&gt;
+&lt;p&gt;This app won&apos;t work, if you are already running a pacman instance.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</source>
+        <translation>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Burada mevcut kurulu ve kurulmaya müsait tüm Linux çekirdekleri hakkında bilgiler bulabilirsiniz.&lt;/p&gt;
+&lt;p&gt;Sol sütundaki kutucuklara tıklayarak çekirdek paketlerini kurabilir ve/ya kaldırabilirsiniz.&lt;/p&gt;
+&lt;p&gt;Halihazırda bir diğer pacman görevi çalışıyorsa, bu uygulama kullanılamaz veya çalışmaz.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Choose</source>
+        <translation>Seç</translation>
+    </message>
+    <message>
+        <source>PkgName</source>
+        <translation>Paket ismi</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Sürüm</translation>
+    </message>
+    <message>
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message>
+        <source>Configure</source>
+        <translation>Yapılandır</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Vazgeç</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>Gerçekleştir</translation>
+    </message>
+    <message>
+        <source>No kernels found!
+Please run `pacman -Sy` to update DB!
+This is needed for the app to work properly</source>
+        <translation>Hiçbir çekirdek bulunamadı!
+Lütfen `pacman -Sy` komutuyla veri tabanını güncelleyin!
+Uygulamanın düzgün çalışabilmesi için bu gereklidir.</translation>
+    </message>
+    <message>
+        <source>Failed to clone repository!
+Please check your internet connection and try again</source>
+        <translation>Repoyu klonlama başarısız oldu!
+Lütfen internet bağlantınızı kontrol edip tekrar deneyin.</translation>
+    </message>
+    <message>
+        <source>Please wait...
+We are preparing configuration window for you
+cloning PKGBUILDs..</source>
+        <translation>Lütfen bekleyin…
+Biçimleme penceresini sizin için hazırlıyoruz
+PKGBUILD'ler klonlanıyor…</translation>
+    </message>
+    <message>
+        <source>Failed to initialize alpm handle (%1)</source>
+        <translation>ALPM işleyicisini başlatım başarısız oldu (%1)</translation>
+    </message>
+    <message>
+        <source>Failed to release alpm handle (%1)</source>
+        <translation>ALPM işleyicisini salım başarısız oldu (%1)</translation>
+    </message>
+    <message>
+        <source>Please wait...
+Initializing kernels..</source>
+        <translation>Lütfen bekleyin…
+Çekirdekler ilk kullanıma hazırlandırılıyor…</translation>
+    </message>
+</context>
+</TS>
