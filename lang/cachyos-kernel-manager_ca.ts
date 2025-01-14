c9cea6dd5d399a6f969e7d5912fe190746a4a3d6
Davidmp <Davidmp1@users.noreply.github.com>
2025-01-14 11:29:59 +0100

    Create cachyos-kernel-manager_ca.ts

---
+++ lang/cachyos-kernel-manager_ca.ts
@@ -0,0 +1,448 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>ConfOptionsPage</name>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="66"/>
+        <source>Enable CachyOS config</source>
+        <translation>Habilita les modificacions del CachyOS</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="95"/>
+        <source>Tweak kernel options prior to a build via nconfig</source>
+        <translation>Ajusteu les opcions del nucli abans d'una compilació mitjançant nconfig</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="124"/>
+        <source>Tweak kernel options prior to a build via menuconfig</source>
+        <translation>Ajusteu les opcions del nucli abans d'una compilació mitjançant menuconfig</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="153"/>
+        <source>Tweak kernel options prior to a build via xconfig</source>
+        <translation>Ajusteu les opcions del nucli abans d'una compilació mitjançant xconfig</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="182"/>
+        <source>Tweak kernel options prior to a build via gconfig</source>
+        <translation>Ajusteu les opcions del nucli abans d'una compilació mitjançant gconfig</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="211"/>
+        <source>Disable NUMA</source>
+        <translation>Inhabilita NUMA</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="240"/>
+        <source>Use Modprobed-db</source>
+        <translation>Usa Modprobed-db</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="269"/>
+        <source>Enable KBUILD_CFLAGS -O3</source>
+        <translation>Habilita KBUILD_CFLAGS -O3</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="298"/>
+        <source>Set performance governor as default</source>
+        <translation>Estableix el governador de rendiment com a predeterminat</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="327"/>
+        <source>Enable TCP_CONG_BBR3</source>
+        <translation>Habilita TCP_CONG_BBR3</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="356"/>
+        <source>Running tick rate</source>
+        <translation>Taxa d'execució</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="385"/>
+        <source>Select tickless</source>
+        <translation>Selecciona tickless</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="414"/>
+        <source>Select preempt</source>
+        <translation>Selecciona preempt</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="646"/>
+        <source>Build the open NVIDIA module</source>
+        <translation>Construeix el mòdul obert d'NVIDIA</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="721"/>
+        <source>Load</source>
+        <translation>Carrega</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="728"/>
+        <source>Save</source>
+        <translation>Desa</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="755"/>
+        <source>Build kernel</source>
+        <translation>Construeix el nucli</translation>
+    </message>
+    <message>
+        <source>Enable multigenerational LRU</source>
+        <translation type="vanished">Habilita LRU multigeneracional</translation>
+    </message>
+    <message>
+        <source>Enable per-VMA locking</source>
+        <translation type="vanished">Habilita el blocatge per VMA</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="472"/>
+        <source>Enable DAMON</source>
+        <translation>Habilita DAMON</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="501"/>
+        <source>CPU compiler optimizations</source>
+        <translation>Optimitzacions del compilador de la CPU</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="530"/>
+        <source>Apply automatic CPU Optimization</source>
+        <translation>Aplica l'optimització automàtica de la CPU</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="559"/>
+        <source>Enable LTO</source>
+        <translation>Habilita LTO</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="588"/>
+        <source>Build the ZFS module</source>
+        <translation>Construeix el mòdul ZFS</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="675"/>
+        <source>Include vmlinux with debug informations/symbols</source>
+        <translation>Inclou vmlinux amb informació/símbols de depuració</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="443"/>
+        <source>Transparent Hugepages</source>
+        <translation>Pàgines enormes transparents</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="748"/>
+        <source>Cancel</source>
+        <translation>Cancel·la</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation type="vanished">Executa</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="33"/>
+        <source>Custom package name</source>
+        <translation>Nom del paquet personalitzat</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="53"/>
+        <source>$pkgbase</source>
+        <translation>$pkgbase</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="617"/>
+        <source>Build the NVIDIA module</source>
+        <translation>Construeix el mòdul d'NVIDIA</translation>
+    </message>
+</context>
+<context>
+    <name>ConfPatchesPage</name>
+    <message>
+        <location filename="../src/conf-patches-page.ui" line="81"/>
+        <source>Add remote patch</source>
+        <translation>Afegeix un pedaç remot</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-patches-page.ui" line="88"/>
+        <source>Add local patch</source>
+        <translation>Afegeix un pedaç local</translation>
+    </message>
+</context>
+<context>
+    <name>ConfWindow</name>
+    <message>
+        <location filename="../src/conf-window.ui" line="17"/>
+        <source>CachyOS Kernel Manager Configure</source>
+        <translation>Gestor de nuclis del CachyOS</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.ui" line="40"/>
+        <source>Options</source>
+        <translation>Opcions</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.ui" line="45"/>
+        <source>Patches</source>
+        <translation>Pedaços</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="457"/>
+        <source>Bore - Burst-Oriented Response Enhancer</source>
+        <translation>Bore - Potenciador de resposta orientat a ràfegues</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="458"/>
+        <source>RC - Release Candidate</source>
+        <translation>RC - Candidat de publicació</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="536"/>
+        <source>Select one or more patch files</source>
+        <translation>Seleccioneu un o més fitxers de pedaç</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="538"/>
+        <source>Patch file (*.patch)</source>
+        <translationFitxer de pedaç (*.patch)</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="558"/>
+        <source>Enter URL patch</source>
+        <translation>Introduïu l'URL del pedaç</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="559"/>
+        <source>Patch URL:</source>
+        <translation>URL del pedaç:</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="682"/>
+        <source>Save file as</source>
+        <translation>Desa el fitxer com a</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="684"/>
+        <location filename="../src/conf-window.cpp" line="701"/>
+        <source>Config file (*.toml)</source>
+        <translation>Fitxer de config. (*.toml)</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="691"/>
+        <source>Failed to save config options to file: %1</source>
+        <translation>Ha fallat desar les opcions de configuració al fitxer %1</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="699"/>
+        <source>Load from</source>
+        <translation>Carrega des de</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="709"/>
+        <source>Failed to load config options from file: %1</source>
+        <translation>Ha fallat carregar les opcions de configuració des del fitxer %1</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="745"/>
+        <source>Config file(%1) is outdated</source>
+        <translation>El fitxer de configuració (%1) està obsolet.</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="459"/>
+        <source>RT - Realtime kernel</source>
+        <translation>RT - Nucli en temps real</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="344"/>
+        <source>Do you want to install build packages?</source>
+        <translation>Voleu instal·lar els paquets de compilació?</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="460"/>
+        <source>RT-Bore</source>
+        <translation>RT-Bore</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="461"/>
+        <source>Sched-Ext - BPF extensible scheduler class</source>
+        <translation>Sched-Ext - Classe de planificador extensible BPF</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="456"/>
+        <source>CachyOS - BORE + SCHED-EXT</source>
+        <translation>CachyOS - BORE + SCHED-EXT</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/km-window.ui" line="17"/>
+        <source>CachyOS Kernel Manager</source>
+        <translation>Gestor de nuclis del CachyOS</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
+&lt;p&gt;You can install and/or uninstall kernel packages using the checkboxes on the leftmost column.&lt;/p&gt;
+&lt;p&gt;This app won&apos;t work, if you are already running a pacman instance.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</source>
+        <translation type="vanished">&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Aquí veureu informació sobre els nuclis de Linux instal·lats i disponibles actualment.&lt;/p&gt;
+&lt;p&gt;Podeu instal·lar o desinstal·lar paquets de nucli usant les caselles de selecció de la columna de l'esquerra.&lt;/p&gt;
+&lt;p&gt;Aquesta aplicació no funcionarà si ja s'executa una instància de pacman.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="64"/>
+        <source>Choose</source>
+        <translation>Trieu</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="69"/>
+        <source>PkgName</source>
+        <translation>Nom del paquet</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="74"/>
+        <source>Version</source>
+        <translation>Versió</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="79"/>
+        <source>Category</source>
+        <translation>Categoria</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="116"/>
+        <source>sched-ext scheduler config</source>
+        <translation>Configuració del planificador sched-ext</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="123"/>
+        <source>Configure</source>
+        <translation>Configura</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="130"/>
+        <source>Cancel</source>
+        <translation>Cancel·la</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="137"/>
+        <source>Execute</source>
+        <translation>Executa</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="227"/>
+        <source>No kernels found!
+Please run `pacman -Sy` to update DB!
+This is needed for the app to work properly</source>
+        <translation>No s'ha trobat cap nucli!
+Si us plau, executeu 'pacman -Sy' per actualitzar la base de dades!
+Això és necessari perquè l'aplicació funcioni correctament.</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="201"/>
+        <source>Failed to clone repository!
+Please check your internet connection and try again</source>
+        <translation>Ha fallat clonar el repositori!
+Si us plau, comproveu la connexió a Internet i torneu-ho a provar.</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="276"/>
+        <location filename="../src/km-window.cpp" line="340"/>
+        <source>Please wait...
+We are preparing configuration window for you
+cloning PKGBUILDs..</source>
+        <translation>Espereu, si us plau...
+Es posa a punt la finestra de configuració.
+Clonació de PKGBUILDs..</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="143"/>
+        <source>Failed to initialize alpm handle (%1)</source>
+        <translation>Ha fallat iniciar l'identificador d'alpm (%1)</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="156"/>
+        <source>Failed to release alpm handle (%1)</source>
+        <translation>Ha fallat alliberar l'identificador d'alpm (%1)</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="361"/>
+        <source>Please wait...
+Initializing kernels..</source>
+        <translation>Espereu, si us plau...
+S'inicien els nuclis...</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="28"/>
+        <source>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
+&lt;p&gt;You can install/uninstall kernel packages using the checkboxes on the leftmost column.&lt;/p&gt;
+&lt;p&gt;This app won&apos;t work if you are already running a pacman instance.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</source>
+        <translation>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Aquí veureu informació sobre els nuclis de Linux instal·lats i disponibles actualment.&lt;/p&gt;
+&lt;p&gt;Podeu instal·lar o desinstal·lar paquets de nucli usant les caselles de selecció de la columna de l'esquerra.&lt;/p&gt;
+&lt;p&gt;Aquesta aplicació no funcionarà si ja s'executa una instància de pacman.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>SchedExtWindow</name>
+    <message>
+        <location filename="../src/schedext-window.ui" line="17"/>
+        <source>CachyOS Configure sched-ext</source>
+        <translation>Configuració de sched-ext del CachyOS</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="40"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Configure sched-ext scheduler:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Configura el planificador sched-ext:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="82"/>
+        <source>Select sched-ext scheduler:</source>
+        <translation>Seleccioneu el planificador sched-ext:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="92"/>
+        <source>Select scheduler profile:</source>
+        <translation>Seleccioneu el perfil del planificador:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="115"/>
+        <source>unknown</source>
+        <translation>desconegut</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="75"/>
+        <source>Running sched-ext scheduler:</source>
+        <translation>Execució del planificador sched-ext:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="122"/>
+        <source>Set sched-ext extra scheduler flags:</source>
+        <translation>Estableix senyals de planificador addicionals de sched-ext:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="176"/>
+        <source>Disable</source>
+        <translation>Inhabilita</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="183"/>
+        <source>Apply</source>
+        <translation>Aplica</translation>
+    </message>
+</context>
+</TS>
