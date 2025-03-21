297edf482dba1750bbb61256071b1f51cfcd9fc0
Daniel Nylander <po@danielnylander.se>
2025-03-21 16:09:00 +0800

    Add Swedish translation

---
+++ lang/cachyos-kernel-manager_sv.ts
@@ -0,0 +1,385 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv_SE">
+    <extra-po-header-language>sv_SE</extra-po-header-language>
+    <extra-po-header-language_team></extra-po-header-language_team>
+    <extra-po-header-last_translator>Daniel Nylander &lt;github@danielnylander.se&gt;</extra-po-header-last_translator>
+    <extra-po-header-po_revision_date></extra-po-header-po_revision_date>
+    <extra-po-header-pot_creation_date></extra-po-header-pot_creation_date>
+    <extra-po-header-project_id_version></extra-po-header-project_id_version>
+    <extra-po-header-x_generator>Poedit 3.5</extra-po-header-x_generator>
+    <extra-po-headers>Project-Id-Version,POT-Creation-Date,PO-Revision-Date,Last-Translator,Language-Team,Language,MIME-Version,Content-Type,Content-Transfer-Encoding,Plural-Forms,X-Language,X-Qt-Contexts,X-Generator</extra-po-headers>
+<context>
+    <name>ConfOptionsPage</name>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="33"/>
+        <source>Custom package name</source>
+        <translation>Anpassat namn på paketet</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="53"/>
+        <source>$pkgbase</source>
+        <translation>$pkgbase</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="66"/>
+        <source>Enable CachyOS config</source>
+        <translation>Aktivera CachyOS-konfiguration</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="95"/>
+        <source>Tweak kernel options prior to a build via nconfig</source>
+        <translation>Justera kärnalternativen före en build via nconfig</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="124"/>
+        <source>Tweak kernel options prior to a build via menuconfig</source>
+        <translation>Justera kärnalternativen före en build via menuconfig</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="153"/>
+        <source>Tweak kernel options prior to a build via xconfig</source>
+        <translation>Justera kärnalternativen före en build via xconfig</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="182"/>
+        <source>Tweak kernel options prior to a build via gconfig</source>
+        <translation>Justera kärnalternativen före en build via gconfig</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="211"/>
+        <source>Disable NUMA</source>
+        <translation>Inaktivera NUMA</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="240"/>
+        <source>Use Modprobed-db</source>
+        <translation>Använd Modprobed-db</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="269"/>
+        <source>Enable KBUILD_CFLAGS -O3</source>
+        <translation>Aktivera KBUILD_CFLAGS -O3</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="298"/>
+        <source>Set performance governor as default</source>
+        <translation>Ställ in Performance Governor som standard</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="327"/>
+        <source>Enable TCP_CONG_BBR3</source>
+        <translation>Aktivera TCP_CONG_BBR3</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="356"/>
+        <source>Running tick rate</source>
+        <translation>Körande tickfrekvens</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="385"/>
+        <source>Select tickless</source>
+        <translation>Välj tickless</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="414"/>
+        <source>Select preempt</source>
+        <translation>Välj preempt</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="443"/>
+        <source>Transparent Hugepages</source>
+        <translation>Transparenta Hugepages</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="472"/>
+        <source>Enable DAMON</source>
+        <translation>Aktivera DAMON</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="501"/>
+        <source>CPU compiler optimizations</source>
+        <translation>Optimering av CPU-kompilatorer</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="530"/>
+        <source>Apply automatic CPU Optimization</source>
+        <translation>Tillämpa automatisk CPU-optimering</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="559"/>
+        <source>Enable LTO</source>
+        <translation>Aktivera LTO</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="588"/>
+        <source>Build the ZFS module</source>
+        <translation>Bygg ZFS-modulen</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="617"/>
+        <source>Build the NVIDIA module</source>
+        <translation>Bygg NVIDIA-modulen</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="646"/>
+        <source>Build the open NVIDIA module</source>
+        <translation>Bygg den öppna NVIDIA-modulen</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="675"/>
+        <source>Include vmlinux with debug informations/symbols</source>
+        <translation>Inkludera vmlinux med felsökningsinformation/symboler</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="721"/>
+        <source>Load</source>
+        <translation>Läs in</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="728"/>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="748"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="755"/>
+        <source>Build kernel</source>
+        <translation>Bygg kärnan</translation>
+    </message>
+</context>
+<context>
+    <name>ConfPatchesPage</name>
+    <message>
+        <location filename="../src/conf-patches-page.ui" line="81"/>
+        <source>Add remote patch</source>
+        <translation>Lägg till fjärrpatch</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-patches-page.ui" line="88"/>
+        <source>Add local patch</source>
+        <translation>Lägg till lokal patch</translation>
+    </message>
+</context>
+<context>
+    <name>ConfWindow</name>
+    <message>
+        <location filename="../src/conf-window.ui" line="17"/>
+        <source>CachyOS Kernel Manager Configure</source>
+        <translation>Hanterare för CachyOS kärnkonfiguration</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.ui" line="40"/>
+        <source>Options</source>
+        <translation>Alternativ</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.ui" line="45"/>
+        <source>Patches</source>
+        <translation>Patchar</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="370"/>
+        <source>Do you want to install build packages?</source>
+        <translation>Vill du installera byggpaket?</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="482"/>
+        <source>CachyOS default scheduler (BORE+Cachy Sauce)</source>
+        <translation>CachyOS standardschemaläggare (BORE+Cachy Sauce)</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="483"/>
+        <source>BORE - Burst-Oriented Response Enhancer</source>
+        <translation>BORE - Burst-orienterad responsförstärkare</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="484"/>
+        <source>RC - Release Candidate</source>
+        <translation>RC - Release Candidate</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="485"/>
+        <source>RT - Realtime kernel</source>
+        <translation>RT - Realtidskärna</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="486"/>
+        <source>RT-Bore</source>
+        <translation>RT-Bore</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="487"/>
+        <source>EEVDF</source>
+        <translation>EEVDF</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="488"/>
+        <source>BMQ (BitMap Queue)</source>
+        <translation>BMQ (BitMap-kö)</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="564"/>
+        <source>Select one or more patch files</source>
+        <translation>Välj en eller flera patchfiler</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="566"/>
+        <source>Patch file (*.patch)</source>
+        <translation>Patch-fil (*.patch)</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="586"/>
+        <source>Enter URL patch</source>
+        <translation>Ange URL till patch</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="587"/>
+        <source>Patch URL:</source>
+        <translation>Patchens URL:</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="710"/>
+        <source>Save file as</source>
+        <translation>Spara fil som</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="712"/>
+        <location filename="../src/conf-window.cpp" line="729"/>
+        <source>Config file (*.toml)</source>
+        <translation>Konfigureringsfil (*.toml)</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="719"/>
+        <source>Failed to save config options to file: %1</source>
+        <translation>Misslyckades med att spara konfigurationsalternativ i fil: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="727"/>
+        <source>Load from</source>
+        <translation>Läs in från</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="737"/>
+        <source>Failed to load config options from file: %1</source>
+        <translation>Misslyckades med att läsa in konfigurationsalternativ från filen: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="773"/>
+        <source>Config file(%1) is outdated</source>
+        <translation>Konfigfilen(%1) är utdaterad</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/km-window.ui" line="17"/>
+        <source>CachyOS Kernel Manager</source>
+        <translation>CachyOS kärnkonfiguration</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="28"/>
+        <source>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
+&lt;p&gt;You can install/uninstall kernel packages using the checkboxes on the leftmost column.&lt;/p&gt;
+&lt;p&gt;This app won&apos;t work if you are already running a pacman instance.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</source>
+        <translation>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Här ser du information om installerade och tillgängliga Linux-kärnor.&lt;/p&gt;
+&lt;p&gt;Du kan installera/avinstallera kärnpaket med hjälp av kryssrutorna i kolumnen längst till vänster.&lt;/p&gt;
+&lt;p&gt;Den här appen fungerar inte om du redan kör en pacman-instans.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="64"/>
+        <source>Choose</source>
+        <translation>Välj</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="69"/>
+        <source>PkgName</source>
+        <translation>PkgName</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="74"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="79"/>
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="116"/>
+        <source>sched-ext scheduler config</source>
+        <translation>sched-ext konfiguration för schemaläggare</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="123"/>
+        <source>Configure</source>
+        <translation>Konfigurera</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="130"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="137"/>
+        <source>Execute</source>
+        <translation>Utför</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="143"/>
+        <source>Failed to initialize alpm handle (%1)</source>
+        <translation>Det gick inte att initiera alpm-handtaget (%1)</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="156"/>
+        <source>Failed to release alpm handle (%1)</source>
+        <translation>Misslyckades med att frigöra alpm-handtaget (%1)</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="201"/>
+        <source>Failed to clone repository!
+Please check your internet connection and try again</source>
+        <translation>Misslyckades med att klona förrådet!
+Kontrollera din internetanslutning och försök igen</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="227"/>
+        <source>No kernels found!
+Please run `pacman -Sy` to update DB!
+This is needed for the app to work properly</source>
+        <translation>Inga kärnor hittades!
+Kör `pacman -Sy` för att uppdatera DB!
+Detta är nödvändigt för att appen ska fungera korrekt</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="276"/>
+        <location filename="../src/km-window.cpp" line="340"/>
+        <source>Please wait...
+We are preparing configuration window for you
+cloning PKGBUILDs..</source>
+        <translation>Vänta...
+Vi förbereder konfigurationsfönstret för dig
+klonar PKGBUILDs..</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="361"/>
+        <source>Please wait...
+Initializing kernels..</source>
+        <translation>Vänta...
+Initiering av kärnor..</translation>
+    </message>
+</context>
+</TS>
