0a031fd6e115f874e698d96fab30b04461f38835
JungHee Lee <36289819+MarongHappy@users.noreply.github.com>
2025-07-30 18:22:36 +0900

    Create cachyos-kernel-manager_ko.ts

---
+++ lang/cachyos-kernel-manager_ko.ts
@@ -0,0 +1,421 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR">
+<context>
+    <name>ConfOptionsPage</name>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="66"/>
+        <source>Enable CachyOS config</source>
+        <translation>CachyOS-구성 활성화</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="95"/>
+        <source>Tweak kernel options prior to a build via nconfig</source>
+        <translation>nconfig를 통해 빌드 전에 커널 옵션 트윅</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="153"/>
+        <source>Tweak kernel options prior to a build via xconfig</source>
+        <translation>xconfig를 통해 빌드 전에 커널 옵션 트윅</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="240"/>
+        <source>Use Modprobed-db</source>
+        <translation>Modprobed-db 사용</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="269"/>
+        <source>Enable KBUILD_CFLAGS -O3</source>
+        <translation>KBUILD_CFLAGS -O3 활성화</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="298"/>
+        <source>Set performance governor as default</source>
+        <translation>성능 조절기를 기본값으로 지정</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="327"/>
+        <source>Enable TCP_CONG_BBR3</source>
+        <translation>TCP_CONG_BBR3 활성화</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="356"/>
+        <source>Running tick rate</source>
+        <translation>실행 틱 레이트</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="385"/>
+        <source>Select tickless</source>
+        <translation>틱리스 선택하기</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="414"/>
+        <source>Select preempt</source>
+        <translation>선점(preempt) 선택하기</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="646"/>
+        <source>Build the open NVIDIA module</source>
+        <translation>오픈 NVIDIA 모듈 빌드</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="721"/>
+        <source>Load</source>
+        <translation>불러오기</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="728"/>
+        <source>Save</source>
+        <translation>저장하기</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="755"/>
+        <source>Build kernel</source>
+        <translation>커널 빌드</translation>
+    </message>
+    <message>
+        <source>Enable multigenerational LRU</source>
+        <translation type="vanished">다세대 LRU 활성화</translation>
+    </message>
+    <message>
+        <source>Enable per-VMA locking</source>
+        <translation type="vanished">VMA별 잠금 활성화</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="472"/>
+        <source>Enable DAMON</source>
+        <translation>DAMON 활성화</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="501"/>
+        <source>CPU compiler optimizations</source>
+        <translation>CPU 컴파일러 최적화</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="530"/>
+        <source>Apply automatic CPU Optimization</source>
+        <translation>자동 CPU 최적화 적용</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="559"/>
+        <source>Enable LTO</source>
+        <translation>LTO 활성화</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="588"/>
+        <source>Build the ZFS module</source>
+        <translation>ZFS 모듈 빌드</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="675"/>
+        <source>Include vmlinux with debug informations/symbols</source>
+        <translation>디버그 정보/기호를 vmlinux에 포함</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="443"/>
+        <source>Transparent Hugepages</source>
+        <translation>투명 거대 페이지</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="748"/>
+        <source>Cancel</source>
+        <translation>취소하기</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation type="vanished">실행하기</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="33"/>
+        <source>Custom package name</source>
+        <translation>사용자 지정 패키지 이름</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="53"/>
+        <source>$pkgbase</source>
+        <translation>$pkgbase</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="617"/>
+        <source>Build the NVIDIA module</source>
+        <translation>NVIDIA 모듈 빌드</translation>
+    </message>
+</context>
+<context>
+    <name>ConfPatchesPage</name>
+    <message>
+        <location filename="../src/conf-patches-page.ui" line="81"/>
+        <source>Add remote patch</source>
+        <translation>원격 패치 추가</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-patches-page.ui" line="88"/>
+        <source>Add local patch</source>
+        <translation>로컬 패치 추가</translation>
+    </message>
+</context>
+<context>
+    <name>ConfWindow</name>
+    <message>
+        <location filename="../src/conf-window.ui" line="17"/>
+        <source>CachyOS Kernel Manager Configure</source>
+        <translation>CachyOS 커널 관리자 구성하기</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.ui" line="40"/>
+        <source>Options</source>
+        <translation>옵션</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.ui" line="45"/>
+        <source>Patches</source>
+        <translation>패치</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="484"/>
+        <source>RC - Release Candidate</source>
+        <translation>RC - 릴리스 후보</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="487"/>
+        <source>EEVDF</source>
+        <translation>EEVDF</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="488"/>
+        <source>BMQ (BitMap Queue)</source>
+        <translation>BMQ (BitMap Queue)</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="563"/>
+        <source>Select one or more patch files</source>
+        <translation>패치 파일 하나 이상 선택</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="565"/>
+        <source>Patch file (*.patch)</source>
+        <translation>패치 파일 (*.patch)</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="585"/>
+        <source>Enter URL patch</source>
+        <translation>URL 패치 입력</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="586"/>
+        <source>Patch URL:</source>
+        <translation>패치 URL:</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="709"/>
+        <source>Save file as</source>
+        <translation>파일 저장 위치</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="711"/>
+        <location filename="../src/conf-window.cpp" line="728"/>
+        <source>Config file (*.toml)</source>
+        <translation>구성 파일 (*.toml)</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="718"/>
+        <source>Failed to save config options to file: %1</source>
+        <translation>구성 옵션을 파일에 저장하지 못함: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="726"/>
+        <source>Load from</source>
+        <translation>불러오기 위치</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="736"/>
+        <source>Failed to load config options from file: %1</source>
+        <translation>파일에서 구성 옵션을 불러오지 못함: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="772"/>
+        <source>Config file(%1) is outdated</source>
+        <translation>구성 파일(%1)이 오래되었습니다</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="485"/>
+        <source>RT - Realtime kernel</source>
+        <translation>RT - 실시간 커널</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="370"/>
+        <source>Do you want to install build packages?</source>
+        <translation>빌드 패키지를 설치하시겠습니까?</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="482"/>
+        <source>CachyOS default scheduler (BORE+Cachy Sauce)</source>
+        <translation>CachyOS 기본 스케줄러 (BORE+Cachy Sauce)</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="483"/>
+        <source>BORE - Burst-Oriented Response Enhancer</source>
+        <translation>BORE - Burst-Oriented Response Enhancer</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-window.cpp" line="486"/>
+        <source>RT-Bore</source>
+        <translation>RT-BORE</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/km-window.ui" line="17"/>
+        <source>CachyOS Kernel Manager</source>
+        <translation>CachyOS 커널 관리자</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="28"/>
+        <source>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
+&lt;p&gt;You can install/uninstall kernel packages using the checkboxes on the leftmost column.&lt;/p&gt;
+&lt;p&gt;This app won&apos;t work if you are already running a pacman instance.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</source>
+        <translation>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;여기에서 현재 설치되어 있고 사용 가능한 Linux 커널에 대한 정보를 확인할 수 있습니다.&lt;/p&gt;
+&lt;p&gt;맨 왼쪽 열의 확인란을 사용하여 커널 패키지를 설치/제거할 수 있습니다.&lt;/p&gt;
+&lt;p&gt;이미 pacman 인스턴스를 실행 중인 경우 이 앱은 작동하지 않습니다.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="64"/>
+        <source>Choose</source>
+        <translation>선택하기</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="69"/>
+        <source>PkgName</source>
+        <translation>패키지이름</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="74"/>
+        <source>Version</source>
+        <translation>버전</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="79"/>
+        <source>Category</source>
+        <translation>범주</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="116"/>
+        <source>sched-ext scheduler config</source>
+        <translation>sched-ext 스케줄러 구성</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="123"/>
+        <source>Configure</source>
+        <translation>구성하기</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="130"/>
+        <source>Cancel</source>
+        <translation>취소하기</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="137"/>
+        <source>Execute</source>
+        <translation>실행하기</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="227"/>
+        <source>No kernels found!
+Please run `pacman -Sy` to update DB!
+This is needed for the app to work properly</source>
+        <translation>커널을 찾을 수 없습니다!
+DB를 업데이트하려면 `pacman -Sy`를 실행하세요!
+앱이 제대로 작동하려면 이 작업이 필요합니다</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="201"/>
+        <source>Failed to clone repository!
+Please check your internet connection and try again</source>
+        <translation>저장소를 복제하지 못했습니다!
+인터넷 연결을 확인하고 다시 시도해 주세요</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="276"/>
+        <location filename="../src/km-window.cpp" line="340"/>
+        <source>Please wait...
+We are preparing configuration window for you
+cloning PKGBUILDs..</source>
+        <translation>잠시만 기다려 주세요...
+PKGBUILD 복제를 위한 구성 창을 준비 중입니다..</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="143"/>
+        <source>Failed to initialize alpm handle (%1)</source>
+        <translation>alpm 핸들(%1)을 초기화하지 못했습니다</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="156"/>
+        <source>Failed to release alpm handle (%1)</source>
+        <translation>alpm 핸들(%1)을 해제하지 못했습니다</translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.cpp" line="361"/>
+        <source>Please wait...
+Initializing kernels..</source>
+        <translation>잠시만 기다려 주세요...
+커널을 초기화하는 중입니다.</translation>
+    </message>
+</context>
+<context>
+    <name>SchedExtWindow</name>
+    <message>
+        <location filename="../src/schedext-window.ui" line="17"/>
+        <source>CachyOS Configure sched-ext</source>
+        <translation>CachyOS sched-ext 구성하기</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="40"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Configure sched-ext scheduler:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;sched-ext 스케줄러 구성:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="82"/>
+        <source>Select sched-ext scheduler:</source>
+        <translation>sched-ext 스케줄러 선택:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="92"/>
+        <source>Select scheduler profile:</source>
+        <translation>스케줄러 프로필 선택:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="115"/>
+        <source>unknown</source>
+        <translation>알 수 없음</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="75"/>
+        <source>Running sched-ext scheduler:</source>
+        <translation>sched-ext 스케줄러 실행:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="122"/>
+        <source>Set sched-ext extra scheduler flags:</source>
+        <translation>sched-ext 추가 스케줄러 플래그 지정:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="176"/>
+        <source>Disable</source>
+        <translation>비활성화</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="183"/>
+        <source>Apply</source>
+        <translation>적용하기</translation>
+    </message>
+</context>
+</TS>
