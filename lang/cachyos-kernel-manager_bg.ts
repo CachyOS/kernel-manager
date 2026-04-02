33caf00f65ee32f5afae2fe109bf000aef28783b
compulsions <momchil.nikolov@outlook.com>
2026-04-02 15:36:46 +0300

    Add Bulgarian Translation

---
+++ lang/cachyos-kernel-manager_bg.ts
@@ -0,0 +1,455 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+<name>ConfOptionsPage</name>
+<message>
+<location filename="../src/conf-options-page.ui" line="66"/>
+<source>Enable CachyOS config</source>
+<translation>Активиране на конфигурацията на CachyOS</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="95"/>
+<source>Tweak kernel options prior to a build via nconfig</source>
+<translation>Настройване на параметрите на ядрото преди изграждане чрез nconfig</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="153"/>
+<source>Tweak kernel options prior to a build via xconfig</source>
+<translation>Настройване на параметрите на ядрото преди изграждане чрез xconfig</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="211"/>
+<source>Disable NUMA</source>
+<translation>Деактивиране на NUMA</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="240"/>
+<source>Use Modprobed-db</source>
+<translation>Използване на Modprobed-db</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="269"/>
+<source>Enable KBUILD_CFLAGS -O3</source>
+<translation>Активиране на KBUILD_CFLAGS -O3</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="298"/>
+<source>Set performance governor as default</source>
+<translation>Настройване на производителния регулатор като стандартен</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="327"/>
+<source>Enable TCP_CONG_BBR3</source>
+<translation>Активиране на TCP_CONG_BBR3</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="356"/>
+<source>Running tick rate</source>
+<translation>Честота на системния таймер</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="385"/>
+<source>Select tickless</source>
+<translation>Избор на режим tickless</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="414"/>
+<source>Select preempt</source>
+<translation>Избор на режим preempt</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="646"/>
+<source>Build the open NVIDIA module</source>
+<translation>Изграждане на отворения модул NVIDIA</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="721"/>
+<source>Load</source>
+<translation>Зареждане</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="728"/>
+<source>Save</source>
+<translation>Запазване</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="755"/>
+<source>Build kernel</source>
+<translation>Изграждане на ядрото</translation>
+</message>
+<message type="vanished">
+<source>Enable multigenerational LRU</source>
+<translation>Активиране на многопоколенски LRU</translation>
+</message>
+<message type="vanished">
+<source>Enable per-VMA locking</source>
+<translation>Активиране на заключване за всяко VMA</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="472"/>
+<source>Enable DAMON</source>
+<translation>Активиране на DAMON</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="501"/>
+<source>CPU compiler optimizations</source>
+<translation>Оптимизации на компилатора за CPU</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="530"/>
+<source>Apply automatic CPU Optimization</source>
+<translation>Прилагане на автоматична оптимизация за CPU</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="559"/>
+<source>Enable LTO</source>
+<translation>Активиране на LTO</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="588"/>
+<source>Build the ZFS module</source>
+<translation>Изграждане на модула ZFS</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="675"/>
+<source>Include vmlinux with debug informations/symbols</source>
+<translation>Включване на vmlinux с дебъг информация/символи</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="443"/>
+<source>Transparent Hugepages</source>
+<translation>Прозрачни големи страници</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="748"/>
+<source>Cancel</source>
+<translation>Отказ</translation>
+</message>
+<message type="vanished">
+<source>Execute</source>
+<translation>Изпълнение</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="33"/>
+<source>Custom package name</source>
+<translation>Потребителско име на пакета</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="53"/>
+<source>$pkgbase</source>
+<translation>$pkgbase</translation>
+</message>
+<message>
+<location filename="../src/conf-options-page.ui" line="617"/>
+<source>Build the NVIDIA module</source>
+<translation>Изграждане на модула NVIDIA</translation>
+</message>
+</context>
+<context>
+<name>ConfPatchesPage</name>
+<message>
+<location filename="../src/conf-patches-page.ui" line="81"/>
+<source>Add remote patch</source>
+<translation>Добавяне на отдалечен пач</translation>
+</message>
+<message>
+<location filename="../src/conf-patches-page.ui" line="88"/>
+<source>Add local patch</source>
+<translation>Добавяне на локален пач</translation>
+</message>
+</context>
+<context>
+<name>ConfWindow</name>
+<message>
+<location filename="../src/conf-window.ui" line="17"/>
+<source>CachyOS Kernel Manager Configure</source>
+<translation>Конфигуратор на ядрата на CachyOS</translation>
+</message>
+<message>
+<location filename="../src/conf-window.ui" line="40"/>
+<source>Options</source>
+<translation>Опции</translation>
+</message>
+<message>
+<location filename="../src/conf-window.ui" line="45"/>
+<source>Patches</source>
+<translation>Пачове</translation>
+</message>
+<message>
+<source>Bore - Burst-Oriented Response Enhancer</source>
+<translation type="vanished">BORE - планировчик за импулсна реакция</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="484"/>
+<source>RC - Release Candidate</source>
+<translation>RC - предварителна версия</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="487"/>
+<source>EEVDF</source>
+<translation>EEVDF</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="488"/>
+<source>BMQ (BitMap Queue)</source>
+<translation>BMQ (битова опашка)</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="563"/>
+<source>Select one or more patch files</source>
+<translation>Изберете един или повече файлове с пачове</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="565"/>
+<source>Patch file (*.patch)</source>
+<translation>Файл с пач (*.patch)</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="585"/>
+<source>Enter URL patch</source>
+<translation>Въведете URL на пач</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="586"/>
+<source>Patch URL:</source>
+<translation>URL на пач:</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="709"/>
+<source>Save file as</source>
+<translation>Запази като</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="711"/>
+<location filename="../src/conf-window.cpp" line="728"/>
+<source>Config file (*.toml)</source>
+<translation>Конфигурационен файл (*.toml)</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="718"/>
+<source>Failed to save config options to file: %1</source>
+<translation>Неуспешно запазване на конфигурационните опции във файл: %1</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="726"/>
+<source>Load from</source>
+<translation>Зареждане от</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="736"/>
+<source>Failed to load config options from file: %1</source>
+<translation>Неуспешно зареждане на конфигурационните опции от файл: %1</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="772"/>
+<source>Config file(%1) is outdated</source>
+<translation>Конфигурационният файл (%1) е остарял</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="485"/>
+<source>RT - Realtime kernel</source>
+<translation>RT - ядро в реално време</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="370"/>
+<source>Do you want to install build packages?</source>
+<translation>Искате ли да инсталирате пакетите за изграждане?</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="482"/>
+<source>CachyOS default scheduler (BORE+Cachy Sauce)</source>
+<translation>Планировчик по подразбиране на CachyOS (BORE + Cachy Sauce)</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="483"/>
+<source>BORE - Burst-Oriented Response Enhancer</source>
+<translation>BORE - планировчик за импулсна реакция</translation>
+</message>
+<message>
+<location filename="../src/conf-window.cpp" line="486"/>
+<source>RT-Bore</source>
+<translation>RT-Bore</translation>
+</message>
+<message type="vanished">
+<source>Sched-Ext - BPF extensible scheduler class</source>
+<translation>Sched-Ext - разширяем планировчик клас BPF</translation>
+</message>
+<message type="vanished">
+<source>CachyOS - BORE + SCHED-EXT</source>
+<translation>CachyOS - BORE + SCHED-EXT</translation>
+</message>
+</context>
+<context>
+<name>MainWindow</name>
+<message>
+<location filename="../src/km-window.ui" line="17"/>
+<source>CachyOS Kernel Manager</source>
+<translation>Мениджър на ядра CachyOS</translation>
+</message>
+<message>
+<source>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
+&lt;p&gt;You can install and/or uninstall kernel packages using the checkboxes on the leftmost column.&lt;/p&gt;
+&lt;p&gt;This app won&apos;t work, if you are already running a pacman instance.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</source>
+<translation type="vanished">&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Тук ще видите информация за инсталираните и наличните Linux ядра.&lt;/p&gt;
+&lt;p&gt;Можете да инсталирате/деинсталирате пакети с ядра, използвайки квадратчетата в най-лявата колона.&lt;/p&gt;
+&lt;p&gt;Това приложение няма да работи, ако вече работи инстанция на pacman.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</translation>
+</message>
+<message>
+<location filename="../src/km-window.ui" line="64"/>
+<source>Choose</source>
+<translation>Избор</translation>
+</message>
+<message>
+<location filename="../src/km-window.ui" line="69"/>
+<source>PkgName</source>
+<translation>Име на пакет</translation>
+</message>
+<message>
+<location filename="../src/km-window.ui" line="74"/>
+<source>Version</source>
+<translation>Версия</translation>
+</message>
+<message>
+<location filename="../src/km-window.ui" line="79"/>
+<source>Category</source>
+<translation>Категория</translation>
+</message>
+<message>
+<location filename="../src/km-window.ui" line="116"/>
+<source>sched-ext scheduler config</source>
+<translation>Конфигурация на планировчика sched-ext</translation>
+</message>
+<message>
+<location filename="../src/km-window.ui" line="123"/>
+<source>Configure</source>
+<translation>Настройка</translation>
+</message>
+<message>
+<location filename="../src/km-window.ui" line="130"/>
+<source>Cancel</source>
+<translation>Отказ</translation>
+</message>
+<message>
+<location filename="../src/km-window.ui" line="137"/>
+<source>Execute</source>
+<translation>Изпълнение</translation>
+</message>
+<message>
+<location filename="../src/km-window.cpp" line="227"/>
+<source>No kernels found!
+Please run `pacman -Sy` to update DB!
+This is needed for the app to work properly</source>
+<translation>Не са намерени ядра!
+Моля, изпълнете `pacman -Sy`, за да обновите базата данни!
+Това е необходимо за правилната работа на приложението.</translation>
+</message>
+<message>
+<location filename="../src/km-window.cpp" line="201"/>
+<source>Failed to clone repository!
+Please check your internet connection and try again</source>
+<translation>Неуспешно клониране на хранилището!
+Моля, проверете интернет връзката си и опитайте отново.</translation>
+</message>
+<message>
+<location filename="../src/km-window.cpp" line="276"/>
+<location filename="../src/km-window.cpp" line="340"/>
+<source>Please wait...
+We are preparing configuration window for you
+cloning PKGBUILDs..</source>
+<translation>Моля, изчакайте...
+Подготвяме прозорец за настройка за вас.
+Клониране на PKGBUILD..</translation>
+</message>
+<message>
+<location filename="../src/km-window.cpp" line="143"/>
+<source>Failed to initialize alpm handle (%1)</source>
+<translation>Неуспешно инициализиране на alpm манипулатор (%1)</translation>
+</message>
+<message>
+<location filename="../src/km-window.cpp" line="156"/>
+<source>Failed to release alpm handle (%1)</source>
+<translation>Неуспешно освобождаване на alpm манипулатор (%1)</translation>
+</message>
+<message>
+<location filename="../src/km-window.cpp" line="361"/>
+<source>Please wait...
+Initializing kernels..</source>
+<translation>Моля, изчакайте...
+Инициализиране на ядрата..</translation>
+</message>
+<message>
+<location filename="../src/km-window.ui" line="28"/>
+<source>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
+&lt;p&gt;You can install/uninstall kernel packages using the checkboxes on the leftmost column.&lt;/p&gt;
+&lt;p&gt;This app won&apos;t work if you are already running a pacman instance.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</source>
+<translation>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Тук ще видите информация за инсталираните и наличните Linux ядра.&lt;/p&gt;
+&lt;p&gt;Можете да инсталирате/деинсталирате пакети с ядра, използвайки квадратчетата в най-лявата колона.&lt;/p&gt;
+&lt;p&gt;Това приложение няма да работи, ако вече работи инстанция на pacman.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</translation>
+</message>
+</context>
+<context>
+<name>SchedExtWindow</name>
+<message>
+<location filename="../src/schedext-window.ui" line="17"/>
+<source>CachyOS Configure sched-ext</source>
+<translation>Настройка на sched-ext в CachyOS</translation>
+</message>
+<message>
+<location filename="../src/schedext-window.ui" line="40"/>
+<source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Configure sched-ext scheduler:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+<translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Настройка на планировчика sched-ext:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+</message>
+<message>
+<location filename="../src/schedext-window.ui" line="82"/>
+<source>Select sched-ext scheduler:</source>
+<translation>Изберете планировчик sched-ext:</translation>
+</message>
+<message>
+<location filename="../src/schedext-window.ui" line="92"/>
+<source>Select scheduler profile:</source>
+<translation>Изберете профил на планировчика:</translation>
+</message>
+<message>
+<location filename="../src/schedext-window.ui" line="115"/>
+<source>unknown</source>
+<translation>неизвестен</translation>
+</message>
+<message>
+<location filename="../src/schedext-window.ui" line="75"/>
+<source>Running sched-ext scheduler:</source>
+<translation>Текущ планировчик sched-ext:</translation>
+</message>
+<message>
+<location filename="../src/schedext-window.ui" line="122"/>
+<source>Set sched-ext extra scheduler flags:</source>
+<translation>Задаване на допълнителни флагове за планировчика sched-ext:</translation>
+</message>
+<message>
+<location filename="../src/schedext-window.ui" line="176"/>
+<source>Disable</source>
+<translation>Деактивиране</translation>
+</message>
+<message>
+<location filename="../src/schedext-window.ui" line="183"/>
+<source>Apply</source>
+<translation>Прилагане</translation>
+</message>
+</context>
+</TS>
